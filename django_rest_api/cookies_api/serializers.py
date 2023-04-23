04559e6950a993216a4411bdebfb56d5b4aa4a72
Leon <Leon@MacBook-Pro.lan>
2023-04-22 23:44:36 -0400

    serializers fix

--- django_rest_api/cookies_api/serializers.py
+++ django_rest_api/cookies_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Cookie
 class CookieSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Cookie # tell django which model to use
-        fields = ('id', 'type', 'cost', 'quantity',) # tell django which fields to include
+        fields = ('id', 'type', 'cost', 'cost_to_make', 'quantity', 'img', 'manufacturer') # tell django which fields to include
